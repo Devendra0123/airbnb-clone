fb6f0ea4cfb25817258f55d5c46dd263e93d3aa1
Devendra0123 <88467890+Devendra0123@users.noreply.github.com>
2021-10-08 14:09:30 +0545

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -39,12 +39,7 @@ function Header({placeholder}) {
     return (
         <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
             <div onClick={() => router.push("/")} className='relative flex items-center h-10 cursor-pointer my-auto'>
-                <Image
-                  src="https://links.papareact.com/qd3"
-                  layout="fill"
-                  objectFit="contain"
-                  objectPosition="left"
-                />
+               <h2>DEVEN</h2>
             </div>
             <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
                 <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" 
